00af46a700a2a62442097053c6e5196a636e0a23
Nick Walsh <mxenergyservices@gmail.com>
2018-07-25 20:41:19 +0100

    Add files via upload

---
+++ scope-2.js
@@ -0,0 +1,18 @@
+// Global (name)
+  // Local (name)
+    // Local
+  // Local
+
+  let name = 'Andrew'
+
+  if (true) {
+      let name = 'Mike'
+
+      if (true) {
+          console.log(name)
+      }
+  }
+
+  if (true) {
+      
+  }
