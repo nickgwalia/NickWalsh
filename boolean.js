f74ae4670c2093bf73844d06be93b0db023bc449
Nick Walsh <mxenergyservices@gmail.com>
2018-07-25 20:37:59 +0100

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,29 @@
+// === - equality operator
+// !== - not equal operator
+// < - less than operator
+// > - greater than operator
+// <= - less than or equal to operator
+// >= - greater than or equal to operator
+
+let temp = 80
+
+if (temp <= 32) {
+    console.log ('it is freezing outside')
+}
+
+if (temp >= 110) {
+    console.log('it is way to hot outside')
+    console.log('testing123')
+}
+//challenge area
+let age = 65
+
+// if 7 or under print message about child pricing
+if (age <= 7) {
+    console.log('you will get a child discount')
+}
+
+//if 65 or older print message about senior discount
+if (age >= 65) {
+    console.log('you will get a senior discount')
+}
