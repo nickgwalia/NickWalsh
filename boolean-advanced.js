8326673e02e716da11254772bd4b68c75da4df24
Nick Walsh <mxenergyservices@gmail.com>
2018-07-25 20:38:28 +0100

    Add files via upload

---
+++ boolean-advanced.js
@@ -0,0 +1,23 @@
+let isAccountLocked = false
+let userRole = 'admin'
+
+if (isAccountLocked) {
+    console.log('is account locked')
+} else if (userRole === 'admin') {
+    console.log('welcome admin')
+} else {
+    console.log('welcome')
+}
+
+//challenge area
+
+let temp = 45
+
+if (temp <= 32) {
+    console.log ('it is freezing outside')
+} else if (temp >= 110) {
+    console.log ('it is hot outside')
+}
+else {
+    console.log('go for it. it is pretty nice')
+}
