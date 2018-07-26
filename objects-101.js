c2764921813e76dac5719d0f080c4ac1df764f26
Nick Walsh <mxenergyservices@gmail.com>
2018-07-26 21:41:00 +0100

    Update objects-101.js

--- objects-101.js
+++ objects-101.js
@@ -5,3 +5,27 @@ let myBook = {
 }
 
 console.log
+
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+console.log (`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log (`${myBook.title} by ${myBook.author}`)
+
+// Challenge area
+
+let me = { 
+    name: 'Dave',
+    age: '27',
+    location: 'London'
+}
+
+console.log(`${me.name} is ${me.age} and lives in ${me.location}.`)
+me.age = me.age +1
+console.log(`${me.name} is ${me.age} and lives in ${me.location}.`)
