9e8236ff19ae9bcae84ec9c35a58a3c46d2d945e
Nick Walsh <mxenergyservices@gmail.com>
2018-07-30 16:31:17 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -6,5 +6,15 @@ console.log('Todo: ${todos[todos.length - 2]}')
 
 // Create an array with five todos
 // You have x todos
-// Print the second to last items -> Todo: do work
+// Print the second to last items -> To do: do work
 
+
+
+const todos = ['Order dog food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
+
+todos.splice(2, 1)
+todos.push('Buy coffee')
+todos.shift()
+
+console.log('You have ${todos.length} todos!')
+console.log(todos)
