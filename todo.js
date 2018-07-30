831960fc8b59fed795d4d7c96ecd66cb4807795c
Nick Walsh <mxenergyservices@gmail.com>
2018-07-30 16:06:47 +0100

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,10 @@
+const todos = ['Order dog food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
+
+console.log('You have ${todos.length} todos!')
+console.log('Todo: ${todos[0]}')
+console.log('Todo: ${todos[todos.length - 2]}')
+
+// Create an array with five todos
+// You have x todos
+// Print the second to last items -> Todo: do work
+
