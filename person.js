f6aa28b97ca2a2226c61b09d15de6c2bdd286aad
Nick Walsh <mxenergyservices@gmail.com>
2018-08-04 15:22:44 +0100

    Update person.js

--- person.js
+++ person.js
@@ -1,11 +1,31 @@
-const Person = function (firstName, lastName, age) {
+// Prototypal Inheritance
+
+const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
+    this.likes = likes
 }
 
-const me = new Person('Dave', 'Smith', 47)
-console.log(me)
+Person.prototype.getBio = function (){
+    let bio = `${this.firstName} is ${this.age}.`
+
+    this.likes.forEach((like)=> {
+        bio += `${this.firstName} likes ${like}.`
+    })
+
+    return bio
+}
+    
+ Person.prototype.setName = function (fullName) {
+     const names = fullName.split(``)
+     this.firstName = names[0]
+     this.lastName = names[1]
+ }
+
+const me = new Person('Dave', 'Smith', 47, ['Teaching', 'Biking'])
+me.setName('Alexis Turner')
+console.log(me.getBio())
 
 const Person2 = new Person('Clancey', 'Turner', 57)
-console.log(Person2)
+console.log(Person2.getBio())
