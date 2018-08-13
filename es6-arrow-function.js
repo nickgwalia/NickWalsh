dfe437e6331ae363452adacf9280d2512ddb831c
Nick Walsh <mxenergyservices@gmail.com>
2018-08-13 17:32:39 +0100

    Add files via upload

---
+++ es6-arrow-function.js
@@ -0,0 +1,28 @@
+// function (x) {
+// return x * x;
+// };
+
+// console.log(square(3));
+
+// // const squareArrow = (x) => {
+// // return x * x;
+// // };
+
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(4));
+
+
+// Challenge - Use arrow functions
+// getFirstName('Mikle Smith') -> "Mike"
+// Create regular arrow function
+// Create arrow function using shorthand syntax
+
+// const getFirstName = (fullName) => {
+//   return fullName.split('')[0];
+// };
+
+const getFirstName = (fullName) => fullName.split('')[0];
+console.log(getFirstName('Mike Smith'));
+
+
